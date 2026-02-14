fe12df09005100746d449b47613930910f00fd70
Meezo06 <moazzibrahim510@gmail.com>
2026-02-14 11:19:59 +0300

    Navigation and Quoting

--- shell.c
+++ shell.c
@@ -6,19 +6,23 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <stdbool.h>
+#include <ctype.h>
+
+typedef unsigned char u8;
+typedef unsigned short u16;
 
 const char* const comms[] = {
 	"exit", "echo", "type", "pwd", "cd"
 };
 
-const unsigned short ARGS_LEN = 50;
-const unsigned short PATHS_LEN = 512;
+const u8 ARGS_LEN = 50;
+const u16 PATHS_LEN = 512;
 
 void echo(const char* const args[]);
 char* type(const char* const comm, bool show);
 bool execute(const char* const args[]);
 void cd(char* const dir, char* cwd);
-
+void trans_line(char* args[], char line[]);
 
 int main(int argc, char *argv[]) {
 	// Flush after every printf
@@ -27,25 +31,21 @@ int main(int argc, char *argv[]) {
 	getcwd(cwd, PATHS_LEN);
 
 
-	while (1) {
+	while (true) {
 		printf("$ ");
 		char line[4096];
-		char* command;
 		char* args[ARGS_LEN];
 		fgets(line, 4096, stdin);
 		line[strcspn(line, "\n")] = '\0';
-		command = strtok(line, " ");
-		args[0] = command;
-		int i = 1;
-		while (i < ARGS_LEN && args[i - 1] != NULL) args[i++] = strtok(NULL, " "); 
-		if (strcmp(command, "exit") == 0) break;
-		else if (strcmp(command, "echo") == 0) echo((const char * const *)args);
-		else if (strcmp(command, "type") == 0) type(args[1], true);
-		else if (strcmp(command, "pwd") == 0) puts(cwd);
-		else if (strcmp(command, "cd") == 0) cd(args[1], cwd);
+		trans_line(args, line);	
+		if (strcmp(args[0], "exit") == 0 || args[0] == NULL) break;
+		else if (strcmp(args[0], "echo") == 0) echo((const char * const *)args);
+		else if (strcmp(args[0], "type") == 0) type(args[1], true);
+		else if (strcmp(args[0], "pwd") == 0) puts(cwd);
+		else if (strcmp(args[0], "cd") == 0) cd(args[1], cwd);
 		else {
 			bool status = execute((const char * const*)args);
-			if (!status) printf("%s: command not found\n", command);
+			if (!status) printf("%s: command not found\n", args[0]);
 		}
 	}
 
@@ -53,13 +53,14 @@ int main(int argc, char *argv[]) {
 }
 
 void echo(const char* const args[]) {
-	for(int i = 1; args[i] != NULL; i++) printf("%s ", args[i]);
+	if (args[0] == NULL) return;
+	for(u8 i = 1; args[i] != NULL; i++) printf("%s ", args[i]);
 	putc('\n', stdout);
 }
 
 char* type(const char const *comm, bool show) {
-	unsigned short comms_len = sizeof(comms) / sizeof(comms[0]);
-	for (int i = 0; i < comms_len; i++) {
+	u8 comms_len = sizeof(comms) / sizeof(comms[0]);
+	for (u16 i = 0; i < comms_len; i++) {
 		if (strcmp(comms[i], comm) == 0) {
 			printf("%s is a shell builtin\n", comm);
 			return NULL;
@@ -133,7 +134,68 @@ bool execute(const char* const args[]) {
 	}
 	else {
 		int status;
-        waitpid(pid, &status, 0); 
+		waitpid(pid, &status, 0); 
 	}
 	return true;
+}
+
+
+void trans_line(char* args[], char line[]) {
+	char* command = strtok(line, " ");
+	args[0] = command;
+	if (command == NULL) return;
+	u8 i = 1;
+	char* tmp = line + strlen(command);
+	*tmp = ' ';
+	char* start = tmp;
+	char* end = start;
+	while (*end != '\0') {
+		while (isspace(*end)) *end++ = '\0';
+		if (*end == '\0') break;
+		start = end;
+		while (!isspace(*end)) {
+			if (*end == '\\') {
+				if (end - start == 0) {
+				       	start++;
+					end += 2;
+					continue;
+				}
+				*end++ = '\0';
+				strcat(start, end);
+				continue;
+			}
+			if ((*end == '\'' || *end == '"') && end - start == 0) {
+				char delim = *end;
+				start++;
+				while (*++end != delim) if (*end == '\0') {
+					printf("Format error\n");
+					return;
+				}
+				*end++ = '\0';
+				if (!isspace(*end)) strcat(start, end--);
+			}
+			else if (*end == '\'' || *end == '"') {
+				char delim = *end;
+				*end = '\0';
+				char* tmp_s = end + 1;
+				while (*++end != delim) if (*end == '\0') {
+					printf("Format error\n");
+					args[0] = NULL;
+					return;
+				}
+				*end++ = '\0';
+				strcat(start, tmp_s);
+				if (!isspace(*end)) {
+					strcat(start, end);
+					end -= 2;
+					*(end + strlen(end) + 1) = '\0';
+				}
+			}
+			else {
+				if (*end++ == '\0') break;
+			}
+		}
+		args[i++] = start;
+	}
+	args[i] = NULL;
 }
